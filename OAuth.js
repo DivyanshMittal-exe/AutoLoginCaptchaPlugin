e599be819d22f0d6b772deb5f8724e0ce3ab1015
Divyansh Mittal <78041366+DivyanshMittal-exe@users.noreply.github.com>
2021-04-21 14:49:00 +0530

    F

--- OAuth.js
+++ OAuth.js
@@ -1,10 +1,4 @@
-var x = document.getElementsByClassName("captcha-image")[0]
-console.log(x);
-var string = OCRAD(x);
-console.log(string);
-var inp = document.getElementsByClassName("form-control captcha-field")[0];
-inp.setAttribute("value", string);
-
+console.log("OAuth Works");
 
 var usrnam = "";
 var psswrd = "";
@@ -14,8 +8,22 @@ chrome.storage.sync.get("username", function (data) {
 chrome.storage.sync.get("password", function (data) {
   psswrd = data.password;
 });
+
+
+
+var x = document.getElementsByClassName("captcha-image")[0]
+console.log(x);
+var string = OCRAD(x);
+console.log(string);
+var inp = document.getElementsByClassName("form-control captcha-field")[0];
+
+
 window.onload = function () {
     document.getElementsByClassName("form-control")[0].setAttribute("value",usrnam);
     document.getElementsByClassName("form-control")[1].setAttribute("value",psswrd);
-    document.getElementsByClassName("btn btn-lg btn-primary btn-block")[0].click();
+    inp.setAttribute("value", string);
+    setTimeout(function(){
+      document.getElementsByClassName("btn btn-lg btn-primary btn-block")[0].click();
+    },500);
+    
 }
--- Webmail.js
+++ Webmail.js
@@ -1,21 +1,26 @@
-var x = document.getElementById("captcha_image")
-var string = OCRAD(x);
-console.log(string);
-
-var inp = document.getElementsByClassName("cc-cpt col-md-5 col-sm-12 col-xs-12 form-control")[0];
-
-
+console.log("Webmail Works");
 
 var usrnam = "";
 var psswrd = "";
 chrome.storage.sync.get("username", function (data) {
   usrnam = data.username;
 });
-usrnam = usrnam + "@iitd.ac.in";
 chrome.storage.sync.get("password", function (data) {
   psswrd = data.password;
 });
+
+
+
+var x = document.getElementById("captcha_image")
+var string = OCRAD(x);
+console.log(string);
+var inp = document.getElementsByClassName("cc-cpt col-md-5 col-sm-12 col-xs-12 form-control")[0];
+
+
 window.onload = function () {
+  console.log(usrnam);
+usrnam = usrnam + "@iitd.ac.in";
+console.log(usrnam);
 document.getElementById("rcmloginuser").setAttribute("value",usrnam);
 document.getElementById("rcmloginpwd").setAttribute("value",psswrd);
 inp.setAttribute("value", string);
